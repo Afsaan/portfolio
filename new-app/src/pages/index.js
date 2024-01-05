ae8392e55fa88758c04547d52c8763048184cf19
kanhaiyapatidar <kanhaiyapatidar94@gmail.com>
2024-01-05 18:28:57 +0530

    add code

---
+++ new-app/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from "@/components/Main"
+export default function Home() {
+  return (
+    <div class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+    
+<Main/>
+    </div>
+  )
+}
